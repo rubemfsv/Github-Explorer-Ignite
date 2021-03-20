076336101fb227fd80df37f5e679c6eb07344592
Rubem <rfsv@ic.ufal.br>
2021-03-20 17:37:37 -0300

    feature: fast refresh in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -16,13 +17,21 @@ module.exports = {
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
+    hot: true,
   },
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel')
+            ].filter(Boolean)
+          }
+        },
       },
       {
         test: /\.scss$/,
@@ -32,8 +41,10 @@ module.exports = {
     ],
   },
   plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin(),
+
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
     }),
-  ],
+  ].filter(Boolean),
 };
