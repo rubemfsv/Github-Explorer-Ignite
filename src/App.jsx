93937b6e331a4368a062ad82a4be17a5e825bfc2
Rubem <rfsv@ic.ufal.br>
2021-03-20 17:04:07 -0300

    feature: create repository component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import RespositoryList from './components/RespositoryList';
 import './styles/global.scss';
 
 export function App() {
-  return <h1>Hello World</h1>;
+  return <RespositoryList />;
 }
